c9fed72879cf7cee7f1756c8360aa7b7e79285f4
wuzhen <janewuwz@gmail.com>
2018-02-10 22:07:11 +0800

    countdown

--- src/example1.js
+++ src/example1.js
@@ -2,8 +2,45 @@ import React from 'react'
 import { componentFromStream } from 'recompose'
 import { Observable } from 'rxjs'
 
-const App = componentFromStream(props$ => {
+let App = componentFromStream(props$ => {
   return Observable.interval(1000).map(i => <div>{i}</div>)
+})
+
+const now = new Date().getTime()
+const format = (time) => {
+  if (time < 0) return '00'
+  return time < 10 ? '0' + time : time
+}
+// countdown
+App = componentFromStream(props$ => {
+  const init = 10
+  return Observable.interval(1000).map(i => {
+    return <div>{init - i < 0 ? 0 : init - i }</div>
+  })
+})
+
+// clock
+App = componentFromStream(props$ => {
+  return Observable.interval(1000).map(i => {
+    const time = new Date(now - i * 1000)
+    const hour = format(time.getHours())
+    const minute = format(time.getMinutes())
+    const second = format(time.getSeconds())
+    return <div>{hour + ':' + minute + ':' + second}</div>
+  }
+  )
+})
+
+// 倒计时 10min
+const duration = 10 * 60
+const speed = 500
+App = componentFromStream(props$ => {
+  return Observable.interval(speed).map(i => {
+    const time = duration - i
+    const minute = format(Math.floor(time / 60))
+    const second = format(time % 60)
+    return <div>{minute + ':' + second}</div>
+  })
 })
 
 export default App
--- src/index.js
+++ src/index.js
@@ -1,5 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './hoc12'
+import './index.css'
+import App from './example1'
+import {Observable} from 'rxjs'
+import {
+	setObservableConfig,
+	componentFromStream,
+	createEventHandler
+} from 'recompose'
+import config from 'recompose/rxjsObservableConfig'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+setObservableConfig(config)
+
+// ReactDOM.render(<App message="I'm jane" speed={1000} />, document.getElementById('root'))
+
+ReactDOM.render(
+  <div
+    style={{
+      marginTop: 40,
+      display: 'flex',
+      justifyContent: 'center',
+      flexDirection: 'column',
+      textAlign: 'center'
+    }}>
+    <App />
+  </div>,
+document.getElementById('root'))
